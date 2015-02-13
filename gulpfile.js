83f142fa06cb28df7697718f94f8e7036cbc0b5e
Mike Francis <mike@benchmark.co.uk>
2015-02-13 10:53:46 +0000

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp         = require('gulp'),
+    connect      = require('gulp-connect'),
+    sass         = require('gulp-sass'),
+    minifycss    = require('gulp-minify-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserify   = require('browserify'),
+    source       = require('vinyl-source-stream')
+    uglify       = require('gulp-uglify');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: ['demo', 'dist'],
+    livereload: true
+  })
+});
+
+gulp.task('css', function() {
+  gulp.src('src/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(connect.reload());
+})
+
+gulp.task('js', function() {
+  return browserify({
+      entries: './src/js/main.js'
+    })
+    .bundle()
+    .pipe(source('main.js'))
+    // .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/scss/**/*.scss', ['css']);
+  gulp.watch('src/js/**/*.js', ['js']);
+});
+
+gulp.task('default', ['connect', 'watch']);
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var $ = require('jquery');
+
+$('.gallery').each(function() {
+
+  console.log($(this));
+
+});
