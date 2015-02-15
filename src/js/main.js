0de1e1d9ace263344ddb2999aa7f53f3a39ba450
Mike Francis <mikeffrancis@gmail.com>
2015-02-15 18:34:53 +0000

    JS tidy

--- src/js/main.js
+++ src/js/main.js
@@ -4,30 +4,23 @@ var $ = require('jquery');
 
 $(function() {
 
-  var $gallery      = $('.gallery'),
-      $galleryInner = $('.gallery-inner', $gallery),
+  var $gallery         = $('.gallery'),
+      $galleryInner    = $('.gallery-inner', $gallery),
+      galleryInnerLeft = 0,
       $galleryItem,
-      galleryHeight = 0,
-      galleryWidth  = 0,
-      $buttonPrev   = $('.gallery-prev', $gallery),
-      $buttonNext   = $('.gallery-next', $gallery),
-      slideWidth    = 0,
-      speed         = 400,
-      easing        = 'swing',
-      startingPos;
-
-  $(window).on('load', function() {
-    galleryInit();
-    $galleryInner.children(':last-child').insertBefore($galleryInner.children(':first-child'));
-  });
-
-  $(window).on('resize', galleryInit);
+      galleryItemWidth = 0,
+      galleryHeight    = 0,
+      galleryWidth     = 0,
+      $buttonPrev      = $('.gallery-prev', $gallery),
+      $buttonNext      = $('.gallery-next', $gallery),
+      speed            = 400,
+      easing           = 'swing';
 
   function galleryInit() {
     galleryHeight = 0;
     galleryWidth  = 0;
-    slideWidth = $galleryInner.children(':first-child').outerWidth(true);
-    $galleryInner.css('left', 0 - slideWidth);
+    galleryItemWidth = $galleryInner.children(':first-child').outerWidth(true);
+    $galleryInner.css('left', 0 - galleryItemWidth);
     $galleryInner.children().each(function() {
       $galleryItem = $(this);
       if ($galleryItem.height() > galleryHeight) {
@@ -39,18 +32,25 @@ $(function() {
     $galleryInner.width(galleryWidth);
   }
 
-  $buttonPrev.on('click', function(e) {
-    startingPos = $galleryInner.position().left;
+  $(window).on('load', function() {
+    galleryInit();
     $galleryInner.children(':last-child').insertBefore($galleryInner.children(':first-child'));
-    $galleryInner.css('left', (startingPos - slideWidth) + 'px');
-    $galleryInner.not(':animated').animate({left: startingPos + 'px'}, speed, easing);
+  });
+
+  $(window).on('resize', galleryInit);
+
+  $buttonPrev.on('click', function(e) {
+    galleryInnerLeft = $galleryInner.position().left;
+    $galleryInner.children(':last-child').insertBefore($galleryInner.children(':first-child'));
+    $galleryInner.css('left', (galleryInnerLeft - galleryItemWidth) + 'px');
+    $galleryInner.not(':animated').animate({left: galleryInnerLeft + 'px'}, speed, easing);
   });
 
   $buttonNext.on('click', function(e) {
-    startingPos = $galleryInner.position().left;
+    galleryInnerLeft = $galleryInner.position().left;
     $galleryInner.children(':first-child').insertAfter($galleryInner.children(':last-child'));
-    $galleryInner.css('left', (startingPos + slideWidth) + 'px');
-    $galleryInner.not(':animated').animate({left: startingPos + 'px'}, speed, easing);
+    $galleryInner.css('left', (galleryInnerLeft + galleryItemWidth) + 'px');
+    $galleryInner.not(':animated').animate({left: galleryInnerLeft + 'px'}, speed, easing);
   });
 
 });
